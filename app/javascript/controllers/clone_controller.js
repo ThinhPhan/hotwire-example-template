f99c309a59645e7af3ee04fc347169f42a846dae
Sean Doyle <sean.p.doyle24@gmail.com>
2022-02-06 23:57:32 -0500

    Appending alerts with Turbo Streams

--- app/javascript/controllers/clone_controller.js
+++ app/javascript/controllers/clone_controller.js
@@ -2,10 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "source" ]
-  static values = { destination: String }
 
-  append() {
-    const destination = document.getElementById(this.destinationValue)
+  append(event) {
+    const destination = event.target
 
     for (const { content } of this.sourceTargets) {
       destination.append(content.cloneNode(true))
